97cc46edd784834eb9fb4286c0b86fa4de97c5e3
Houndrace <Hound322@yandex.ru>
2023-02-28 10:08:50 +0300

    normix

---
+++ PurchasingDepartment/Models/ProcurementModel/ЭлектроннаяПочта.cs
@@ -0,0 +1,31 @@
+namespace PurchasingDepartment.Models.ProcurementModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class ЭлектроннаяПочта
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public ЭлектроннаяПочта()
+        {
+            Поставщик = new HashSet<Поставщик>();
+            Сотрудник = new HashSet<Сотрудник>();
+        }
+
+        [Key]
+        public int Код { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string АдресПочты { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Поставщик> Поставщик { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Сотрудник> Сотрудник { get; set; }
+    }
+}
---
+++ PurchasingDepartment/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchasingDepartment.ViewModels {
+    internal class MainViewModel : BaseViewModel {
+        // Fields
+        private int pageNumber = 1;
+        // Constuctor
+        MainViewModel() {
+
+        }
+        // Properties to be binded
+    }
+}
